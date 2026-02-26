6aa3dd7ce5cec86659f50de7b4eec12458fadb65
m0_69551548 <yanxu52@huawei.com>
2026-02-26 10:03:37 +0800

    feat:support custom hitls path to build pqcp
    Co-authored-by: Tom<yanxu52@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !34 merge feature/support-hybrid-env into main
    
    feat:support custom hitls path to build pqcp
    
    Created-by: m0_69551548
    Commit-by: Tom
    Merged-by: fly2x
    Description: feat:support custom hitls path to build pqcp
    
    See merge request: openHiTLS/pqcp!34

--- build_pqcp.sh
+++ build_pqcp.sh
@@ -23,6 +23,8 @@ OPTIONS=""
 BUILD_TYPE="Release"
 ENABLE_ASAN="OFF"
 OPENHITLS_DIR="platform/openhitls"
+LIB_TYPE="SHARED"
+CUSTOM_HITLS_DIR=""
 SECURE_C_DIR="platform/openhitls/platform/Secure_C"
 
 # 解析参数
@@ -37,6 +39,18 @@ while [[ $# -gt 0 ]]; do
             ENABLE_ASAN="ON"
             OPTIONS+="asan "
             shift
+            ;;
+        static)
+            LIB_TYPE="STATIC"
+            shift
+            ;;
+        --hitls_dir)
+            if [[ -z "$2" || "$2" == -* ]]; then
+                echo "错误: --hitls_dir 选项需要一个参数"
+                exit 1
+            fi
+            CUSTOM_HITLS_DIR="$2"
+            shift 2
             ;;
         *)
             echo "未知参数: $1"
@@ -80,13 +94,20 @@ if [ ! -d "${PQCP_ROOT_DIR}/${OPENHITLS_DIR}" ]; then
 fi
 
 build_depend_code
+if [ -n "$CUSTOM_HITLS_DIR" ]; then
+    HITLS_ROOT_PATH="$CUSTOM_HITLS_DIR"
+else
+    HITLS_ROOT_PATH="${PQCP_ROOT_DIR}/${OPENHITLS_DIR}"
+fi
 
 # 运行CMake配置
 echo "生成构建系统..."
 cmake .. \
     -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
     -DENABLE_ASAN=${ENABLE_ASAN} \
-    -DCMAKE_PREFIX_PATH="${PQCP_ROOT_DIR}/${OPENHITLS_DIR};${PQCP_ROOT_DIR}/${SECURE_C_DIR}"
+    -DCMAKE_PREFIX_PATH="${PQCP_ROOT_DIR}/${OPENHITLS_DIR};${PQCP_ROOT_DIR}/${SECURE_C_DIR}" \
+    -DHITLS_ROOT_PATH="${HITLS_ROOT_PATH}" \
+    -DPQCP_LIB_TYPE=${LIB_TYPE}
 
 # 编译项目
 echo "开始编译..."
